d3a40a310372642a5a4ccb5c7b981d91e7a6ed48
k9wan <user@LAPTOP-0B6VMU37>
2019-06-14 21:55:24 +0900

    add execute method and some methods

--- src/Macro.java
+++ src/Macro.java
@@ -4,16 +4,43 @@ import java.util.regex.*;	//Matcher MatchResult Pattern PatterSyntaxException
 public class Macro {
 	
 	public static ArrayList<Macro> macroList = new ArrayList<Macro>(30);
+	private String name;				//이름
 	private String pattern;				//인식할 패턴
 	private String pattern_result;		//바뀔 패턴
 	private boolean useRegex;			//정규표현식 사용여부
 	private String helpDoc;				//어떤 패턴을 어떻게 바꾸는지 알려줄 수 있는 독스트링을 위한 필드;
+	private boolean enabled;			//사용 여부
 	
 	public Macro(String original, String result, boolean useRegex) {
 		// TODO Auto-generated constructor stub
 		this.pattern = original;
 		this.pattern_result = result;
 		this.useRegex = useRegex;
+	}
+	
+	public Macro(String name, String original, String result, boolean useRegex, String helpDoc, boolean enabled) {
+		// TODO Auto-generated constructor stub
+		this.name = name;
+		this.pattern = original;
+		this.pattern_result = result;
+		this.useRegex = useRegex;
+		this.helpDoc = helpDoc;
+		this.enabled = enabled;
+	}
+	
+	public Macro(String name, String original, String result, boolean useRegex, String helpDoc) {
+		// TODO Auto-generated constructor stub
+		this(name, original, result, useRegex, helpDoc, true);
+	}
+
+	public Macro(String name, String original, String result, boolean useRegex) {
+		// TODO Auto-generated constructor stub
+		this(name, original, result, useRegex, "helpDoc not Added");
+	}
+
+	public Macro(String name, String original, String result, String helpDoc) {
+		// TODO Auto-generated constructor stub
+		this(name, original, result, false, helpDoc);
 	}
 	
 	public Macro(String original, String result) {
@@ -28,10 +55,90 @@ public class Macro {
 			return targetStr.replace(pattern, pattern_result);
 		}
 	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getPattern() {
+		return pattern;
+	}
+	
+	public String getPatternResult() {
+		return pattern_result;
+	}
+	
+	public boolean getUseRegex() {
+		return useRegex;
+	}
+	
+	public String getHelpDoc() {
+		return helpDoc;
+	}
+	
+	public boolean getEnabled() {
+		return enabled;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setPattern(String pattern) {
+		this.pattern = pattern;
+	}
+	
+	public void setPatternResult(String pattern_result) {
+		this.pattern_result = pattern_result;
+	}
+	
+	public void setUseRegex(boolean useRegex) {
+		this.useRegex = useRegex;
+	}
+	
+	public void setHelpDoc(String helpDoc) {
+		this.helpDoc = helpDoc;
+	}
+	
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+	
+	public String info() {
+		return "name: "+name+"\nhelp:\n"+helpDoc+"\nenabled: "+(enabled?"enabled":"disabled")+"\nfrom:\n"+pattern+"\nto:\n"+pattern_result+"\nuse regex: "+useRegex;
+	}
+	
+	public static void predefine() {	//will be invoked when program starts;
+		macroList.add(new Macro(
+				"malloc2d",
+				"(\\w+)( ?\\* ?\\* ?)(\\w+)( ?= ?)\\w?alloc\\[(\\w+)\\]\\[(\\w+)\\];",
+				"$1$2$3$4malloc($5*sizeof *$3 + $6*$5*sizeof **$3);\nfor(int i=0;i<$5;i++)\n{\n\t$3[i]$4($1 *)($3+$5)+i*n;\n}\n",
+				true,
+				"Use to allocate 2D array.\nExample: uint_8 ** matrix = malloc[6][cols];"));
+	}
+	
+	public static String execute(String str) {
+		for(Macro m:macroList) {
+			if(m.enabled) {
+				str = m.replace(str);
+			}
+		}
+		return str;
+	}
+	
+	public static Macro findByName(String name) {
+		for(Macro m:macroList) {
+			if(m.name.equals(name)) {
+				return m;
+			}
+		}
+		return null;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		test();
+		//test();
+		predefine();
 	}
 	
 	private static void test() {	//will be used to see how regex and replacement works-테스트용;
